950ce9fdfbc35ff4088fb7cc26e401c02622a3a9
FIC <edusousa59@gmail.com>
2017-10-06 18:52:40 -0300

    bacon

--- Program.cs
+++ Program.cs
@@ -2,11 +2,27 @@
 
 namespace ProjetoForm
 {
+
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string Genero=CadastroGenero();
+            double RG=CadastroRG();
+        }
+        static string CadastroGenero()
+        {
+            string X;
+            Console.WriteLine("Digite seu Gênero: ");
+            X=Console.ReadLine();
+            return X;
+        }
+        static double CadastroRG()
+        {
+            double X;
+            Console.WriteLine("Digite seu Gênero: ");
+            X=double.Parse(Console.ReadLine());
+            return X;
         }
     }
 }
