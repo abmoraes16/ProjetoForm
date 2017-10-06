6047fde535f049b77493532a2cf66f3d7999f97e
FIC <a.bmoraes@outlook.com>
2017-10-06 20:30:31 -0300

    MostraDados

--- Program.cs
+++ Program.cs
@@ -5,65 +5,110 @@ namespace ProjetoForm
 
     class Program
     {
+            static string Nome;
+            static string Genero;
+            static string RG;
+            static string CPF;
+            static string Endereco;
+            static string EstadoCivil;
+            static string Profissao;
+            static string LocalNascimento;
+            static string Idade;
+
         static void Main(string[] args)
         {
-            string Nome=CadastroNome();
-            string Genero=CadastroGenero();
-            string RG=CadastroRG();
-            string CPF=CadastroCPF();
-            string Endereco=CadastroEndereco();
-            string EstadoCivil=CadastroEstadoCivil();
-            string Profissao=CadastroProfissao();
+            Nome=CadastroNome();
+            Genero=CadastroGenero();
+            RG=CadastroRG();
+            CPF=CadastroCPF();
+            Endereco=CadastroEndereco();
+            EstadoCivil=CadastroEstadoCivil();
+            Profissao=CadastroProfissao();
+            LocalNascimento=CadastroLocalNascimento();
+            Idade=CadastroIdade();
+
+            MostraDados();
         }
         
         static string CadastroNome(){
-            string nome;
-            nome = Console.ReadLine();
-            return nome;
+            string X;
+            Console.Write("Digite seu Nome: ");
+            X = Console.ReadLine();
+            return X;
         }
 
         static string CadastroGenero()
         {
             string X;
-            Console.WriteLine("Digite seu Gênero: ");
+            Console.Write("Digite seu Gênero: ");
             X=Console.ReadLine();
             return X;
         }
         static string CadastroRG()
         {
             string X;
-            Console.WriteLine("Digite seu RG: ");
+            Console.Write("Digite seu RG: ");
             X=Console.ReadLine();
             return X;
         }
         static string CadastroCPF()
         {
             string X;
-            Console.WriteLine("Digite seu CPF: ");
+            Console.Write("Digite seu CPF: ");
             X=Console.ReadLine();
             return X;
         }
 
         static string CadastroEndereco(){
-            string endereco;
-            Console.WriteLine("Digite seu endereco: ");
-            endereco = Console.ReadLine();
-            return endereco;
+            string X;
+            Console.Write("Digite seu endereco: ");
+            X = Console.ReadLine();
+            return X;
         }
 
         static string CadastroEstadoCivil()
         {
             string X;
-            Console.WriteLine("Digite seu Estado Civil: ");
+            Console.Write("Digite seu Estado Civil: ");
             X=Console.ReadLine();
             return X;
         }
         static string CadastroProfissao()
         {
             string X;
-            Console.WriteLine("Digite sua Profissão: ");
+            Console.Write("Digite sua Profissão: ");
             X=Console.ReadLine();
             return X;
+        }
+
+        static string CadastroLocalNascimento()
+        {
+            string X;
+            Console.Write("Digite seu Local de Nascimento: ");
+            X=Console.ReadLine();
+            return X;
+        }
+
+        static string CadastroIdade()
+        {
+            string X;
+            Console.Write("Digite sua Idade: ");
+            X=Console.ReadLine();
+            return X;
+        }
+
+        static void MostraDados()
+        {
+            Console.WriteLine("\n\nRESULTADO:\n");
+            Console.WriteLine("Nome: "+Nome);
+            Console.WriteLine("Nome: "+Genero);
+            Console.WriteLine("Nome: "+RG);
+            Console.WriteLine("Nome: "+CPF);
+            Console.WriteLine("Nome: "+Endereco);
+            Console.WriteLine("Nome: "+EstadoCivil);
+            Console.WriteLine("Nome: "+Profissao);
+            Console.WriteLine("Nome: "+LocalNascimento);
+            Console.WriteLine("Nome: "+Idade);
         }
     }
 }
